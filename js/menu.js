a9b4d8cafb440f7dd4f3d5676fb9a676b6d08c0a
Viktor Himlih <vhimlihmail@gmail.com>
2023-03-03 02:45:39 +0300

    adaptive

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const buttonNode = document.querySelector('.menu__btn');
+const menuListNode = document.querySelector('.menu__list')
+const bodyNode = document.querySelector('body')
+
+function switchMenu(){
+	if(menuListNode.classList.contains('menu__list_active')){
+		menuListNode.classList.remove('menu__list_active')
+		bodyNode.classList.remove('no-scroll')
+	} else {
+		menuListNode.classList.add('menu__list_active')
+		bodyNode.classList.add('no-scroll')
+	}
+}
+
+buttonNode.addEventListener('click', switchMenu)
+
